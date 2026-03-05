778843cecc228e1271d76e97ff0185bbef96113f
Kameleon <yusuf.dadi7@gmail.com>
2026-03-05 00:33:48 -0500

    Code still needs correction but vould still try to do the convergence analysis with it

--- Devoir_2/fdm_implicite.py
+++ Devoir_2/fdm_implicite.py
@@ -10,78 +10,73 @@ D_EFF = 1e-10 # Diffusivité effective (m^2/s)
 C_E = 20.0 # Concentration à la surface (mol/m^3)
 K = 4e-9 # (1/s)
 
-def solve_fdm_implicite(N, T, dt):
+def solve_fdm_implicite(N, T, N_t):
     """
     Résout l'équation de diffusion 1D radiale instationnaire par différences finies implicites.
 
     Args:
         N (int): Nombre total de nœuds.
         T (float): Temps total de simulation.
-        dt (float): Pas de temps.
+        N_t (int): Nombre de pas de temps.
 
     Returns:
         tuple: Vecteur des concentrations calculées.
     """
     # Création du maillage
-    dr = R / (N - 1)
     r = np.linspace(0, R, N)
+    t = np.linspace(0, T, N_t)
+    dr = r[1] - r[0]
+    dt = t[1] - t[0]
     
     # Initialisation matricielle (Systeme A*C = b)
     A = np.zeros((N, N))
-    b = np.zeros(N)
-    const_source = S / Deff
-
-    # --- 1. CONDITION FRONTIÈRE AU CENTRE (r=0, i=0) ---
-    if schema == 'D':
-        # Question D : 
-        # Schéma décentré avant (Forward) 
-        A[0, 0] = -1.0
-        A[0, 1] = 1.0
-        b[0] = 0
-    elif schema == 'E':
-        # Question E : 
-        # Intègre la symétrie (flux nul) 
-        A[0, 0] = -3.0
-        A[0, 1] = 4.0
-        A[0, 2] = -1.0
-        b[0] = 0.0
-
-    # --- 2. NŒUDS INTÉRIEURS (0 < r < R) ---
-    for i in range(1, N - 1):
-        ri = r[i]
-        
-        # Facteurs communs
-        inv_dr2 = 1.0 / (dr**2)
-        inv_rdr = 1.0 / (ri * dr)
-        
-        if schema == 'D':
-            # Question D : Dérivée première décentrée avant (Forward) [cite: 55]
-            
-            # C_{i-1}
-            A[i, i-1] = inv_dr2
-            # C_{i}
-            A[i, i]   = -2.0 * inv_dr2 - inv_rdr
-            # C_{i+1}
-            A[i, i+1] = inv_dr2 + inv_rdr
-            
-        elif schema == 'E':
-            # Question E : Dérivée première centrée [cite: 67]
-            
-            # C_{i-1}
-            A[i, i-1] =  (inv_dr2 - 0.5 * inv_rdr)
-            # C_{i}
-            A[i, i]   = -2.0 * inv_dr2 
-            # C_{i+1}
-            A[i, i+1] =  (inv_dr2 + 0.5 * inv_rdr)
-            
-        b[i] = S/Deff
-
-    # --- 3. CONDITION FRONTIÈRE SURFACE (r=R, i=N-1) ---
-    # Condition de Dirichlet : C = Ce 
-    A[N-1, N-1] = 1.0
-    b[N-1] = Ce
+    b = np.zeros((N))
+    C = np.zeros((N, N_t)) # Sert aussi pour la condition initiale nulle
     
-    # Résolution
-    C = solve(A, b)
+    # Loop temporel
+    for i in range(N_t - 1):
+        # Condition frontière au centre (r=0, i=0)
+        A[0, 0] = -3
+        A[0, 1] = 4
+        A[0, 2] = -1
+        b[0] = 2 * dr *np.sin(t[i+1])
+        
+        # Condition frontière à la surface (r=R, i=N-1)
+        A[-1, -1] = 1
+        b[-1] = (t[i+1]) * np.cos(R) + np.exp(R) * np.sin(t[i+1]) # Nouvelle condition de Dirichlet à la surface avec MMS
+        
+        # Nœuds intérieurs
+        for j in range(1, N - 1):
+            terme_source_MMS = -1 * D_EFF * (-(t[i+1])*np.cos(r[j]) + np.exp(r[j])*np.sin(t[i+1]) + (-(t[i+1])*np.sin(r[j]) + np.exp(r[j])*np.sin(t[i+1]))/r[j]) + K * ((t[i+1])*np.cos(r[j]) + np.exp(r[j])*np.sin(t[i+1])) + np.exp(r[j]) * np.cos(t[i+1]) + np.cos(r[j])
+            A[j, j-1] = -1 *dt * D_EFF * (1 - dr/(2*r[j]))
+            A[j, j] = dr**2 + 2 * dt * D_EFF + K * dr**2 * dt
+            A[j, j+1] = -1 * dt * D_EFF * (1 + dr/(2*r[j]))
+            b[j] = dr**2 * C[j, i] + dr**2 * dt *terme_source_MMS
+        
+        # Résolution du système linéaire pour le pas de temps ti+1  
+        C[:, i+1] = solve(A, b)
     
     return C
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    N = 10
+    T = 4e9
+    N_t = 100
+
+    # Domaines 
+    r_dom = np.linspace(0, R, N)
+    t_dom = np.linspace(0, T, N_t)
+    ti, ri = np.meshgrid(t_dom, r_dom)
+
+    # Evaluation de la solution numérique sur le domaine
+    C = solve_fdm_implicite(N, T, N_t)
+
+    # Tracage de la solution numérique
+    plt.figure()
+    plt.contourf(ri, ti, C, levels=20)
+    plt.colorbar()
+    plt.title('Solution Numerique')
+    plt.xlabel('Rayon r (m)')
+    plt.ylabel('Temps t (s)')
+    plt.show()
